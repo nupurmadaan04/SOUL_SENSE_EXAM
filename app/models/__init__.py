ab618e6dbd3dc01f76078bb1c6dcdf670997447e
harveenkaur282-web <harveenkaur282@gmail.com>
2026-01-16 05:58:37 +0530

    Fix models/__init__.py: import Base from db and all models to avoid circular import

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,14 @@
-from sqlalchemy.orm import declarative_base
 
-Base = declarative_base()
+from app.db import Base
+
+from .reflection import UserReflection
+from .user import User
+from .score import Score
+from .journal_entry import JournalEntry
+from .satisfaction_record import SatisfactionRecord
+from .assessment_result import AssessmentResult
+from .response import Response
+from .question import Question
+from .question_cache import QuestionCache
+from .statistics_cache import StatisticsCache
+from .user_settings import UserSettings
